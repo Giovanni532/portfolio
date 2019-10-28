3ef19848a5a781e8ed4d90552ebd2d764695d613
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-28 13:32:12 +0100

    add handling change in competences

--- src/FR/Pages/Competences.js
+++ src/FR/Pages/Competences.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
 const Competences = () => {
-    return <p>Competences</p>
+    const [competence, setCompetence] = React.useState(true)
+    const [certificat, setCertificat] = React.useState(false)
+
+    const handlingCompetence = () => {
+        setCertificat(false)
+        setCompetence(true)
+    }
+
+    const handlingCertificat = () => {
+        setCompetence(false)
+        setCertificat(true)
+    }
+
+    return (<div>
+        <div><p onClick={() => handlingCompetence()}>Competences</p><p onClick={() => handlingCertificat()}>Certificat</p></div>
+        {competence === true ?
+            <p>Competences</p>
+            :
+            certificat === true ?
+                <p>Certificat</p>
+                :
+                <p>Competences</p>
+        }
+    </div>
+    )
 }
 
 export default Competences
