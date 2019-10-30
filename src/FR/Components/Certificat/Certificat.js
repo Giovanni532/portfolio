c5e1ef84f1213812fe072ad5934cd72f95e38b97
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-30 17:53:34 +0100

    add a new certificat

--- src/FR/Components/Certificat/Certificat.js
+++ src/FR/Components/Certificat/Certificat.js
@@ -5,23 +5,39 @@ import imageOP from '../../Assets/Certificat/imageOP.png'
 const Certificat = () => {
     const certificatArray = [{
         id: 1,
+        title: "OpenClassRoom",
         name: "Concevez votre site web avec PHP et MySQL",
         className: "title-certificat-php",
         img: imageOP,
-        skills: "Compétences acquises : PHP, MySQL, gestion d'une base de donnée MySQL"
-    }]
+        skills: "PHP, MySQL, gestion d'une base de donnée MySQL"
+    },
+    {
+        id: 2,
+        title: "OpenClassRoom",
+        name: "Apprenez à coder avec JavaScript",
+        className: "title-certificat-js",
+        img: imageOP,
+        skills: "JavaScript, Animations, Dynamisez une page web"
+    }
+]
 
     return (
         <Container>
             <Row>
                 <Col className="certificats">
-                    {certificatArray.map(certificat => 
-                    <div key={certificat.id}>
-                        <h2 className={certificat.className}>{certificat.name}</h2>
+                    {certificatArray.map(certificat =>
                         <div>
-                           <img src={certificat.img} alt={certificat.name}/> 
+                            <h2 className={certificat.className}>{certificat.name}</h2>
+                            <div className="space-point"></div>
+                            <div class="card-certificat">
+                                <img src={certificat.img} alt={certificat.name} />
+                                <div class="card-certificat-infos">
+                                    <h2 style={{textAlign: 'left'}}>{certificat.title}</h2>
+                                    <p style={{textAlign: 'left'}}>Compétences acquises : {certificat.skills}</p>
+                                </div>
+                            </div>
+                            <div className="space-point"></div>
                         </div>
-                    </div>
                     )}
                 </Col>
             </Row>
