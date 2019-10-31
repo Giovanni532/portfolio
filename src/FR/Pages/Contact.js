f3777cebaae73b98e01c00c270fd14a2e88142c2
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-31 10:49:37 +0100

    start the contact pages

--- src/FR/Pages/Contact.js
+++ src/FR/Pages/Contact.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Container, Form } from 'react-bootstrap'
 
 const Contact = () => {
-    return <p>Contact</p>
+    const [email, setEmail] = React.useState('')
+    const [subject, setSubject] = React.useState('')
+    const [message, setMessage] = React.useState('')
+    const [succes, setSuccess] = React.useState(null)
+    const [error, setError] = React.useState('')
+
+    return (
+        <Container>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+            </Form>
+        </Container>
+    )
 }
 
 export default Contact
