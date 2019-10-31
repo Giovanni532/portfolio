c6456b9120f74d03ec5c535973710c532d41eb69
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-31 10:42:34 +0100

    lack of inspiration for projects postpones it until later

--- src/FR/Components/Project/Project.js
+++ src/FR/Components/Project/Project.js
@@ -1,29 +1,69 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap'
-import jeffrey from '../../Assets/Project/jeffrey.png'
+// import { Carousel } from 'react-bootstrap'
+// import jeffrey from '../../Assets/Project/jeffrey.png'
+// import otactuWebApp from '../../Assets/Project/otactu-web-app.png'
+// import easyColoc from '../../Assets/Project/easy-coloc.png'
+// import otacu from '../../Assets/Project/otacu.png'
+// import b8OfHope from '../../Assets/Project/b8ofhope.png'
+// import dreamDrive from '../../Assets/Project/dream-drive.png'
+// import gic from '../../Assets/Project/gic.png'
+// import friperie from '../../Assets/Project/ze-friperie.png'
 
 export default function ControlledCarousel() {
-    const [index, setIndex] = React.useState(0);
-    const [direction, setDirection] = React.useState(null);
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-        setDirection(e.direction);
-    };
+    // const slideData = [
+    //     {
+    //         id: 1,
+    //         title: "Jeffrey APP",
+    //         classNameTitle: "title-jeffrey",
+    //         img: jeffrey
+    //     },
+    //     {
+    //         id: 2,
+    //         title: "Otactu-React",
+    //         classNameTitle: "title-jeffrey",
+    //         img: otactuWebApp
+    //     },
+    //     {
+    //         id: 3,
+    //         title: "Easy-coloc",
+    //         classNameTitle: "title-jeffrey",
+    //         img: easyColoc
+    //     },
+    //     {
+    //         id: 4,
+    //         title: "Otaku",
+    //         classNameTitle: "title-jeffrey",
+    //         img: otacu
+    //     },
+    //     {
+    //         id: 5,
+    //         title: "GIC",
+    //         classNameTitle: "title-jeffrey",
+    //         img: gic
+    //     },
+    //     {
+    //         id: 6,
+    //         title: "b8 of hope",
+    //         classNameTitle: "title-jeffrey",
+    //         img: b8OfHope
+    //     },
+    //     {
+    //         id: 7,
+    //         title: "Dream Drive",
+    //         classNameTitle: "title-jeffrey",
+    //         img: dreamDrive
+    //     },
+    //     {
+    //         id: 8,
+    //         title: "Ze Friperie",
+    //         classNameTitle: "title-jeffrey",
+    //         img: friperie
+    //     },
+    // ]
+
 
     return (
-        <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
-            <Carousel.Item>
-                <h3 className="title-jeffrey">Jeffrey APP</h3>
-                <img
-                    className="d-block w-100 image-project"
-                    src={jeffrey}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
+        <h2>a suivre</h2>
+    )
 }
