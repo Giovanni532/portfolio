36bc7922948a86319e011babd4020aab2db0d1db
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-11-11 12:24:18 +0100

    create skelette for home

--- src/FR/Pages/Maison.js
+++ src/FR/Pages/Maison.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import profilImg from '../Assets/Maison/profil.jpg'
 
 const Maison = () => {
-    return <div>
-        <p>Bonjour monde</p>
-    </div>
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <div className="profil">
+                        <div className="profil-image">
+                            <img src={profilImg} alt="Giovanni Salcuni Profil" className="img" />
+                        </div>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="profil">
+                        <div>
+                            <h1>Salut ! je suis Salcuni Giovanni </h1>
+                            <h1>Développeur web FullStack</h1>
+                        </div>
+                        <div>
+                            <p>Texte qui viendra ici ....</p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default Maison
