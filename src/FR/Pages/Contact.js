a447aeb87c1ad44defd7097e00c59ce273acfcde
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-12-16 11:28:17 +0100

    design and window dimensions with helper

--- src/FR/Pages/Contact.js
+++ src/FR/Pages/Contact.js
@@ -1,25 +1,49 @@
 import React from 'react'
 import { Container, Form, Row, Col } from 'react-bootstrap'
+import useWindowDimensions from '../../helper/UseWindowDimensions'
 
 const Contact = () => {
     const [contacted, setContacted] = React.useState(false)
-
-    const closeContacted = () => {
-        setContacted(false)
-    }
+    const { width } = useWindowDimensions();
 
     return (
         <Container>
-            {contacted ? <FormContact closeContacted={closeContacted} /> : null}
-            <Row style={{textAlign: 'center'}}>
+            {contacted ? <FormContact /> : null}
+            <Row style={{ textAlign: 'center' }}>
+                {width <= 750 ?
+                    contacted ?
+                    <Col>
+                        <div onClick={() => setContacted(false)} className="fade-in-bottom close-email">
+                            <i id="close" className="fas fa-times"></i>
+                            <p>Fermer le formulaire</p>
+                        </div>
+                    </Col>
+                    :
+                    null
+                    :
+                    null
+                }
                 <Col>
-                    <div onClick={() => setContacted(true)} className="contact-email">
+                    <div onClick={() => setContacted(true)} className={contacted ? "contact-email-3" : "contact-email"}>
                         <i id="email" className="fas fa-envelope"></i>
                         <p>Me contacter</p>
                     </div>
                 </Col>
+                {width >= 750 ?
+                    contacted ?
+                    <Col>
+                        <div onClick={() => setContacted(false)} className="fade-in-bottom close-email">
+                            <i id="close" className="fas fa-times"></i>
+                            <p>Fermer le formulaire</p>
+                        </div>
+                    </Col>
+                    :
+                    null
+                    :
+                    null
+                }
                 <Col>
-                    <div className="contact-linkedin">
+                    <div className={contacted ? "contact-linkedin-3" : "contact-linkedin"}>
                         <i id="linkedin" className="fab fa-linkedin"></i>
                         <p><a id="link-linkedin" href="https://www.linkedin.com/in/giovanni-salcuni/" target="_blank">Mon linkedin</a></p>
                     </div>
@@ -29,7 +53,7 @@ const Contact = () => {
     )
 }
 
-const FormContact = ({ closeContacted }) => {
+const FormContact = () => {
     const [email, setEmail] = React.useState('')
     const [subject, setSubject] = React.useState('')
     const [message, setMessage] = React.useState('')
@@ -56,10 +80,9 @@ const FormContact = ({ closeContacted }) => {
     }
 
     return (
-        <div>
+        <div className="fade-in-bottom">
             <Container>
                 <h2 className="title-contact">Contact</h2>
-                <div onClick={() => closeContacted()}>fermer</div>
                 {succes === false ?
                     <Form onSubmit={e => handleSubmit(e)} style={{ textAlign: "center" }} className="contact">
                         <Row style={{ paddingBottom: 20 }}>
@@ -73,8 +96,8 @@ const FormContact = ({ closeContacted }) => {
                         <Form.Group>
                             <Form.Control as="textarea" rows="3" placeholder="Votre message ..." value={message} onChange={e => setMessage(e.target.value)} />
                         </Form.Group>
-                        <button type="submit" className="contact-submit">Submit form</button>
-                        {error}
+                        <button type="submit" className="contact-submit">Envoyer le formulaire</button>
+                        <p style={{ paddingTop: 10 }}>{error}</p>
                     </Form>
                     :
                     <div>
