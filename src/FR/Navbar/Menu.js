dfe59016792419630b99aa1c26dfe06d54114e47
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-12-17 10:04:30 +0100

    change the navbar go to design this

--- src/FR/Navbar/Menu.js
+++ src/FR/Navbar/Menu.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import Scrollchor from 'react-scrollchor';
-import { Nav, Navbar } from 'react-bootstrap'
 
 const Menu = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">Home</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Scrollchor to="#competences" className="navbar-brand">Compétences</Scrollchor>
-                    <Scrollchor to="#projet" className="navbar-brand">Projet</Scrollchor>
-                    <Scrollchor to="#contact" className="navbar-brand">Contact</Scrollchor>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light" data-toggle="affix">
+            <div className="mx-auto d-sm-flex d-block flex-sm-nowrap">
+                <a className="navbar-brand" href="/">Brand</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse text-center" id="navbarsExample11">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Scrollchor to="#competences" className="nav-link">Mes compétences </Scrollchor>
+                        </li>
+                        <li className="nav-item">
+                            <Scrollchor to="#projet" className="nav-link">Mes projets </Scrollchor>
+                        </li>
+                        <li className="nav-item">
+                            <Scrollchor to="#contact" className="nav-link">Contact</Scrollchor>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
